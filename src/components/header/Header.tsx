8720550ea40ed51053d1731570ec61bd8fe8fae7
John <johnwendellllave682@gmail.com>
2024-03-08 16:01:46 +0900

    fix: resolve styling issues in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,13 @@ import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <header className={styles.container}>
-      <span className={styles.logo}>Simon</span>
-      <div className={styles.items}>
-        <button className={styles.button}>Register</button>
-        <button className={styles.button}>Login</button>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <span className={styles.logo}>Simon</span>
+        <div className={styles.items}>
+          <button className={styles.button}>Register</button>
+          <button className={styles.button}>Login</button>
+        </div>
       </div>
     </header>
   );
