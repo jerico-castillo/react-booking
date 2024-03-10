d8e30eb86a432bb01312933f6983c09585dfadf9
John <johnwendellllave682@gmail.com>
2024-03-10 17:28:35 +0900

    feat: add propertyList component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import styles from "./home.module.css";
 
 const Home = () => {
@@ -8,6 +9,8 @@ const Home = () => {
       <Navbar />
       <div className={styles.container}>
         <Featured />
+        <h1 className={styles.title}>Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   );
