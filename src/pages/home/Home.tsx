92d8561cf0f92dfa91c99cae6cc7c54fd9c0ff4f
John <johnwendellllave682@gmail.com>
2024-03-11 13:56:34 +0900

    feat: add featuredProperties component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import styles from "./home.module.css";
@@ -11,6 +12,8 @@ const Home = () => {
         <Featured />
         <h1 className={styles.title}>Browse by property type</h1>
         <PropertyList />
+        <h1 className={styles.title}>Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
