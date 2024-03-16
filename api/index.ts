d0d1b845a604c5018da25abd8a5c7ff22c8c199e
John <johnwendellllave682@gmail.com>
2024-03-16 16:04:42 +0900

    feat: add MongoDB connection

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,38 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app: express.Application = express();
 
+const connect = async () => {
+  if (process.env.MONGO) {
+    try {
+      await mongoose.connect(process.env.MONGO);
+      console.log("Connected to mongoDB");
+    } catch (error) {
+      throw error;
+    }
+  } else {
+    console.error(
+      "Missing MONGO environment variable. Please set it before running the application."
+    );
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("hello first request!");
+});
+
 app.listen(8800, () => {
-  console.log("Connected to Serverdwqdq");
+  connect();
+  console.log("Connected to Server");
 });
