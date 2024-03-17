9b18800f38c1ad6e6c7173c00654f05431444262
John <johnwendellllave682@gmail.com>
2024-03-17 14:29:28 +0900

    feat: Implement Hotel CRUD controllers
    - Implement Hotel CRUD controllers
    - Separate hotel logic into controllers
    - Create `createHotel`, `updateHotel`, `deleteHotel`, `getHotel`, `getHotels` functions

---
+++ api/controllers/hotel.ts
@@ -0,0 +1,69 @@
+import express, { NextFunction } from "express";
+import Hotel from "../models/Hotel";
+
+export const createHotel = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateHotel = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteHotel = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getHotel = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getHotels = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/hotels.ts
+++ api/routes/hotels.ts
@@ -1,7 +1,28 @@
-import express from "express";
+import express, { NextFunction } from "express";
+import Hotel from "../models/Hotel";
+import { createError } from "../utils/error";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel";
 
 const hotelsRouter: express.Router = express.Router();
 
-hotelsRouter.post("/", (req: express.Request, res: express.Response) => {});
+// CREATE
+hotelsRouter.post("/", createHotel);
 
+// UPDATE
+hotelsRouter.put("/:id", updateHotel);
+
+//DELETE
+hotelsRouter.delete("/:id", deleteHotel);
+
+// GET
+hotelsRouter.get("/:id", getHotel);
+
+// GET ALL
+hotelsRouter.get("/", getHotels);
 export default hotelsRouter;
