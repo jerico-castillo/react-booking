a45fd8d570fb5d36589200f56e92d95f78451849
John <johnwendellllave682@gmail.com>
2024-03-18 17:31:55 +0900

    feat: add user login with JWT authentication

--- api/controllers/auth.ts
+++ api/controllers/auth.ts
@@ -2,6 +2,7 @@ import express, { NextFunction } from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/User";
 import { createError } from "../utils/error";
+import jwt from "jsonwebtoken";
 
 export const register = async (
   req: express.Request,
@@ -40,9 +41,16 @@ export const login = async (
     if (isPasswordCorret)
       return next(createError("400", "Wrong password or username"));
 
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT || ""
+    );
     const { password, isAdmin, ...otherDetails } = user._doc;
 
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
