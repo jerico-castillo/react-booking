1006e356fb6659fa3cde261033cfdaaf4c56074a
John <johnwendellllave682@gmail.com>
2024-03-18 15:07:10 +0900

    feat: add users router

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user";
 
 const usersRouter: express.Router = express.Router();
+
+// UPDATE
+usersRouter.put("/:id", updateUser);
+
+//DELETE
+usersRouter.delete("/:id", deleteUser);
+
+// GET
+usersRouter.get("/:id", getUser);
+
+// GET ALL
+usersRouter.get("/", getUsers);
 
 export default usersRouter;
