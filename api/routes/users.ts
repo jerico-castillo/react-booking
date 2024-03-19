b765dd8367bdf47e8a4186c847c9395feb876192
John <johnwendellllave682@gmail.com>
2024-03-19 23:40:52 +0900

    feat: add auth middleware (verifyUser, verifyAdmin) to user routes

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,18 +1,43 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verityToken";
 
 const usersRouter: express.Router = express.Router();
 
+// usersRouter.get(
+//   "/checkauthentication",
+//   verifyToken,
+//   (req: express.Request, res: express.Response, next: NextFunction) => {
+//     res.send(`"hello user, you are logged in!"`);
+//   }
+// );
+
+// usersRouter.get(
+//   "/checkuser/:id",
+//   verifyUser,
+//   (req: express.Request, res: express.Response, next: NextFunction) => {
+//     res.send("hello user, you are logged in and you cal delete you account");
+//   }
+// );
+
+// usersRouter.get(
+//   "/checkadmin/:id",
+//   verifyAdmin,
+//   (req: express.Request, res: express.Response, next: NextFunction) => {
+//     res.send("hello admin, you are logged in and you can delete all account");
+//   }
+// );
+
 // UPDATE
-usersRouter.put("/:id", updateUser);
+usersRouter.put("/:id", verifyUser, updateUser);
 
 //DELETE
-usersRouter.delete("/:id", deleteUser);
+usersRouter.delete("/:id", verifyUser, deleteUser);
 
 // GET
-usersRouter.get("/:id", getUser);
+usersRouter.get("/:id", verifyUser, getUser);
 
 // GET ALL
-usersRouter.get("/", getUsers);
+usersRouter.get("/", verifyAdmin, getUsers);
 
 export default usersRouter;
