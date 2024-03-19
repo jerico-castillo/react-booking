55cbf90c5655188985b373fdc8b216c5d42ce149
John <johnwendellllave682@gmail.com>
2024-03-19 15:56:00 +0900

    feat: improved verifyToken function
    - Improved callback function processing logic in verifyToken function
    - Activate callback function by updating verifyUser and verifyAdmin middleware

--- api/utils/verityToken.ts
+++ api/utils/verityToken.ts
@@ -2,23 +2,60 @@ import express, { NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { createError } from "./error";
 
-interface IGetUserAuthInfoRequest extends express.Request {
-  user: any; // You can refine the type of 'user' based on your needs
-}
-
 export const verifyToken = (
   req: express.Request,
   res: express.Response,
-  next: NextFunction
+  next: NextFunction,
+  callback?: (
+    req: express.Request,
+    res: express.Response,
+    next: NextFunction
+  ) => void
 ) => {
   const token = req.cookies.access_token;
   if (!token) {
     return next(createError("401", "You are not authenticated!"));
   }
-
   jwt.verify(token, process.env.JWT || "", (err: any, user: any) => {
     if (err) return next(createError("403", "Token is not valid!"));
-    (req as IGetUserAuthInfoRequest).user = user;
+
+    req.user = user;
+    if (callback) {
+      callback(req, res, next);
+    }
+
+    // if (req.params.id && req.user.id !== req.params.id) {
+    //   return next(createError("403", "You are not authorized!"));
+    // }
+
     next();
+  });
+};
+
+export const verifyUser = (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  verifyToken(req, res, next, (req, _res, next) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError("403", "You are not authorized!"));
+    }
+  });
+};
+
+export const verifyAdmin = (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  verifyToken(req, res, next, (req, _res, next) => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError("403", "You are not authorized!"));
+    }
   });
 };
