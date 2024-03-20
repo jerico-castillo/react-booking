49a48540be20c62f054b252750587e29fb1f0b9f
John <johnwendellllave682@gmail.com>
2024-03-21 00:47:12 +0900

    feat: add room router

--- api/routes/rooms.ts
+++ api/routes/rooms.ts
@@ -1,5 +1,27 @@
-import express from "express";
+import express, { NextFunction } from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room";
+import { verifyAdmin } from "../utils/verityToken";
 
 const roomsRouter: express.Router = express.Router();
 
+// CREATE
+roomsRouter.post("/", verifyAdmin, createRoom);
+
+// UPDATE
+roomsRouter.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+roomsRouter.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// GET
+roomsRouter.get("/:id", getRoom);
+
+// GET ALL
+roomsRouter.get("/", getRooms);
 export default roomsRouter;
