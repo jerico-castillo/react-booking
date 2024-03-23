50b59b14a127cf27d3865e4ea65bb404a336129f
John <johnwendellllave682@gmail.com>
2024-03-23 23:45:13 +0900

    feat: Make logo clickable with React Router Link

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <span className={styles.logo}>Simon</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className={styles.logo}>Simon</span>
+        </Link>
         <div className={styles.items}>
           <button className={styles.button}>Register</button>
           <button className={styles.button}>Login</button>
