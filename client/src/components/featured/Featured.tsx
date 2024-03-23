956b31bf777dbce4f8c5405aac24cf091e66fb0a
John <johnwendellllave682@gmail.com>
2024-03-23 23:35:08 +0900

    feat: Fetch and display hotel counts by city using React Query

--- client/src/components/featured/Featured.tsx
+++ client/src/components/featured/Featured.tsx
@@ -1,43 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import styles from "./featured.module.css";
+import { fetchHotelCountByCity } from "../../hooks/http";
 
 const Featured = () => {
-  return (
-    <div className={styles.featured}>
-      <div className={styles.item}>
-        <img
-          src="https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className={styles.img}
-        />
-        <div className={styles.titles}>
-          <h1>Dublin</h1>
-          <h2>123 properties</h2>
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["hotelsByCity"],
+    queryFn: () =>
+      fetchHotelCountByCity("/hotels/countByCity?cities=berlin,madrid,london"),
+  });
+
+  let content;
+
+  if (isPending) {
+    content = <p>Loading please wait</p>;
+  }
+
+  if (data) {
+    content = (
+      <>
+        <div className={styles.item}>
+          <img
+            src="https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+          <div className={styles.titles}>
+            <h1>Berlin</h1>
+            <h2>{data[0]} properties</h2>
+          </div>
         </div>
-      </div>
-      <div className={styles.item}>
-        <img
-          src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className={styles.img}
-        />
-        <div className={styles.titles}>
-          <h1>Austin</h1>
-          <h2>532 properties</h2>
+        <div className={styles.item}>
+          <img
+            src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+          <div className={styles.titles}>
+            <h1>Madrid</h1>
+            <h2>{data[1]} properties</h2>
+          </div>
         </div>
-      </div>
-      <div className={styles.item}>
-        <img
-          src="https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className={styles.img}
-        />
-        <div className={styles.titles}>
-          <h1>Reno</h1>
-          <h2>533 properties</h2>
+        <div className={styles.item}>
+          <img
+            src="https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+          <div className={styles.titles}>
+            <h1>London</h1>
+            <h2>{data[2]} properties</h2>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
+  return <div className={styles.featured}>{content}</div>;
 };
 
 export default Featured;
