822fa977767919fb83765d65e2d408e5d83f55cd
John <johnwendellllave682@gmail.com>
2024-03-25 18:23:12 +0900

    feat: Display hotel information and link to details

--- client/src/components/searchItem/SearchItem.tsx
+++ client/src/components/searchItem/SearchItem.tsx
@@ -1,37 +1,40 @@
+import { Link } from "react-router-dom";
+import { Hotel } from "../../types/hotelType";
 import styles from "./searchItem.module.css";
 
-const SearchItem = () => {
+const SearchItem = (props: { item: Hotel }) => {
+  const { photos, name, desc, distance, rating, cheapestPirce, _id, ...other } =
+    props.item;
+
   return (
     <div className={styles.searchItem}>
-      <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
-        alt=""
-        className={styles.img}
-      />
+      {photos && <img src={photos[0]} alt="" className={styles.img} />}
       <div className={styles.desc}>
-        <h1 className={styles.title}>Tower Street Apartments</h1>
-        <span className={styles.distance}>500m from center</span>
+        <h1 className={styles.title}>{name}</h1>
+        <span className={styles.distance}>{distance}m from center</span>
         <span className={styles.taxiOp}>Free airport taxi</span>
         <span className={styles.subTitle}>
           Studio Apartment with Air conditioning
         </span>
-        <span className={styles.features}>
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </span>
+        <span className={styles.features}>{desc}</span>
         <span className={styles.cancelOp}>Free cancellation </span>
         <span className={styles.cancelOpSubtitle}>
           You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className={styles.details}>
-        <div className={styles.rating}>
-          <span>Excellent</span>
-          <button>8.9</button>
-        </div>
+        {rating && (
+          <div className={styles.rating}>
+            <span>Excellent</span>
+            <button>{rating}</button>
+          </div>
+        )}
         <div className={styles.detailTexts}>
-          <span className={styles.price}>$112</span>
+          <span className={styles.price}>${cheapestPirce}</span>
           <span className={styles.taxOp}>Includes taxes and fees</span>
-          <button className={styles.checkButton}>See availability</button>
+          <Link to={`/hotels/${_id}`}>
+            <button className={styles.checkButton}>See availability</button>
+          </Link>
         </div>
       </div>
     </div>
