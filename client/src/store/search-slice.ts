b07a813a180aec14366ff3009f6d76cfcb8a2e6a
John <johnwendellllave682@gmail.com>
2024-03-29 16:54:04 +0900

    fead: Add search slice for travel search functionality

---
+++ client/src/store/search-slice.ts
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface Range {
+  startDate: string;
+  endDate: string;
+  key: string;
+}
+
+// interface Range {
+//   startDate: Date | undefined;
+//   endDate: Date | undefined;
+//   key: string;
+// }
+
+type SliceState = {
+  destination?: string;
+  dates: Range[];
+  options: {
+    adult?: number;
+    children?: number;
+    room: number;
+  };
+};
+
+const initialState: SliceState = {
+  destination: undefined,
+  dates: [
+    {
+      startDate: "",
+      endDate: "",
+      key: "selection",
+    },
+  ],
+  options: {
+    adult: undefined,
+    children: undefined,
+    room: 1,
+  },
+};
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    search(state, action: PayloadAction<SliceState>) {
+      const { destination, dates, options } = action.payload; // 아직 뭐가 들어올지 모르기에 미정
+      state.destination = destination;
+      state.dates = dates;
+      state.options = options;
+    },
+    resetSearch(state) {
+      state.destination = undefined;
+      state.dates = [];
+      state.options = {
+        adult: undefined,
+        children: undefined,
+        room: undefined,
+      };
+    },
+  },
+});
+
+export const { search, resetSearch } = searchSlice.actions;
+
+export default searchSlice.reducer;
