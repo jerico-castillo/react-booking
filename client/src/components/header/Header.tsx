b4f5dd646c8084e33bfa297c4adc6eb5826d568d
John <johnwendellllave682@gmail.com>
2024-03-31 18:55:39 +0900

    feat: Add user display to header based on auth state

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
+import { useAppSelector } from "../../typedHooks/hooks";
 
 const Header = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className={styles.logo}>Simon</span>
         </Link>
-        <div className={styles.items}>
-          <button className={styles.button}>Register</button>
-          <button className={styles.button}>Login</button>
-        </div>
+        {user ? (
+          user.username
+        ) : (
+          <div className={styles.items}>
+            <button className={styles.button}>Register</button>
+            <button className={styles.button}>Login</button>
+          </div>
+        )}
       </div>
     </header>
   );
