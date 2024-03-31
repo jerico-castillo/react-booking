ef85b9796fef9870ecd5123b5590b6810f3857ca
John <johnwendellllave682@gmail.com>
2024-03-31 18:37:46 +0900

    feat: Add auth reducer to Redux store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./search-slice";
+import authReducer from "./auth-slice";
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    auth: authReducer,
   },
 });
 
